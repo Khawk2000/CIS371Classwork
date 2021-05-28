a465185e07bd26b3d392f29db2bdf81fae2a5c6d
Khawk2000 <hawkikee@mail.gvsu.edu>
2021-05-28 20:36:33 +0000

    Completed JobFinder

--- Module3project/jobFinder.js
+++ Module3project/jobFinder.js
@@ -1,5 +1,12 @@
 #! usr/bin/env node
 
+/*
+ * Module 3 Project
+ * JobFinder
+ * Keegan Hawkins
+ */
+
+
 const readline = require('readline').createInterface({
 		input: process.stdin,
 		output: process.stdout
