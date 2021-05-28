7c067fb25b92bd29605fee7d1cf55e3ff54486e7
Khawk2000 <hawkikee@mail.gvsu.edu>
2021-05-28 20:30:04 +0000

    Completed JobFinder

---
+++ Module3project/jobFinder.js
@@ -0,0 +1,37 @@
+#! usr/bin/env node
+
+const readline = require('readline').createInterface({
+		input: process.stdin,
+		output: process.stdout
+});
+
+const fetch = require('node-fetch');
+
+
+
+function doThing(code){
+	fetch('https://api.zippopotam.us/us/' + code)
+		.then(response => response.json())
+		.then(data => {
+			let longitude = data.places[0].longitude;
+			let latitude = data.places[0].latitude;
+			getJobs(longitude, latitude);
+		});
+		
+}
+
+function getJobs(longitude, latitude){
+	fetch('https://jobs.github.com/positions.json?lat=' + latitude + '&long=' + longitude)
+		.then(response => response.json())
+		.then(data => {
+			data.forEach(element => console.log('\n\n' + element.location + '\n ================== \n' + element.company + '\n' + element.type + '\n' + element.title + '\n \n' + element.description + '\n ==================\n'));
+		});
+	readline.close();
+	
+}
+
+readline.question('Where are you looking for a job?\nZip Code:', code => {doThing(code);});
+
+
+
+
