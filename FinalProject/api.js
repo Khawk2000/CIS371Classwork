18b3f3e7df5744fbba4556edba8a9ca6f59e151d
Khawk2000 <hawkikee@mail.gvsu.edu>
2021-06-19 21:53:10 +0000

    Final project updated

---
+++ FinalProject/api.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+const express = require('express'), app = express(), port = process.env.PORT || 3500;
+app.use(express.json());
+
+const cors = require('cors');
+app.use(cors());
+
+var database = require('./database.js');
+database.openDB();
+
+var users = require('./user.js');
+app.use('/', users);
+
+var posts = require('./post.js');
+app.use('/', posts);
+
+
+app.listen(port);
+console.log('Listening on port: ' + port);
+
+
